d69d24b49c788500e0074f6ad145c42d72ddc012
Boris <bgonzalez@ing.uchile.cl>
2018-01-18 11:13:03 -0300

    :ambulance: It works

--- lambdas/chauchometro.js
+++ lambdas/chauchometro.js
@@ -1,10 +1,8 @@
-
+const fetch = require('node-fetch')
 // Route the incoming request based on type (LaunchRequest, IntentRequest,
 // etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = function (event, context) {
   try {
-    console.log('event.session.application.applicationId=' + event.session.application.applicationId)
-
     if (event.session.application.applicationId !== 'amzn1.ask.skill.60596b34-7971-41f7-aaf7-53327e89562e') {
       context.fail('Invalid Application ID')
     }
@@ -15,16 +13,16 @@ exports.handler = function (event, context) {
 
     if (event.request.type === 'LaunchRequest') {
       onLaunch(event.request,
-                     event.session,
-                     function callback (sessionAttributes, speechletResponse) {
-                       context.succeed(buildResponse(sessionAttributes, speechletResponse))
-                     })
+                   event.session,
+                   function callback (sessionAttributes, speechletResponse) {
+                     context.succeed(buildResponse(sessionAttributes, speechletResponse))
+                   })
     } else if (event.request.type === 'IntentRequest') {
       onIntent(event.request,
-                     event.session,
-                     function callback (sessionAttributes, speechletResponse) {
-                       context.succeed(buildResponse(sessionAttributes, speechletResponse))
-                     })
+                   event.session,
+                   function callback (sessionAttributes, speechletResponse) {
+                     context.succeed(buildResponse(sessionAttributes, speechletResponse))
+                   })
     } else if (event.request.type === 'SessionEndedRequest') {
       onSessionEnded(event.request, event.session)
       context.succeed()
@@ -34,86 +32,93 @@ exports.handler = function (event, context) {
   }
 }
 
-/**
- * Called when the session starts.
- */
 function onSessionStarted (sessionStartedRequest, session) {
   console.log('onSessionStarted requestId=' + sessionStartedRequest.requestId +
-                ', sessionId=' + session.sessionId)
+              ', sessionId=' + session.sessionId)
 }
 
-/**
- * Called when the user launches the skill without specifying what they want.
- */
 function onLaunch (launchRequest, session, callback) {
   console.log('onLaunch requestId=' + launchRequest.requestId +
-                ', sessionId=' + session.sessionId)
+              ', sessionId=' + session.sessionId)
 
-    // Dispatch to your skill's launch.
   getWelcomeResponse(callback)
 }
 
-/**
- * Called when the user specifies an intent for this skill.
- */
 function onIntent (intentRequest, session, callback) {
-  console.log('onIntent requestId=' + intentRequest.requestId +
-                ', sessionId=' + session.sessionId)
-
-  let intentName = intentRequest.intent.name
-
+  const intent = intentRequest.intent
+  const intentName = intent.name
+  console.log('ON INTENT: ', intentName)
   if (intentName === 'ChauchaGetValueIntent') {
-    console.log('So you want to know how much the chaucha is now')
+    getChauchaValue(callback)
   } else {
-    console.log('Invalid intent')
+    throw 'Invalid intent'
   }
 }
 
 /**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
+* Called when the user ends the session.
+* Is not called when the skill returns shouldEndSession=true.
+*/
+
 function onSessionEnded (sessionEndedRequest, session) {
   console.log('onSessionEnded requestId=' + sessionEndedRequest.requestId +
-                ', sessionId=' + session.sessionId)
-    // Add cleanup logic here
+              ', sessionId=' + session.sessionId)
+  // Add cleanup logic here
 }
 
-// --------------- Functions that control the skill's behavior -----------------------
+function getChauchaValue (callback) {
+  console.log('GETTING CHAUCHA VALUE')
+  let query = `{
+    market(code: "CHACLP"){
+      lastTrade{
+        price
+      }
+    }
+  }`
+
+  try {
+    fetch('https://api.orionx.io/graphql', {
+      method: 'POST',
+      body: JSON.stringify({query: query}),
+      headers: { 'Content-Type': 'application/json' }
+    })
+  .then(res => res.json())
+  .then(json => onSuccess(callback, json.data.market.lastTrade.price))
+  } catch (e) {
+    onError(callback)
+  }
+}
+
+function onSuccess (callback, chauchaValue) {
+  const sessionAttributes = {}
+  const cardTitle = 'Chaucha Value'
+  const speechOutput = 'The chaucha value as of now is ' + chauchaValue + ' CLP'
+  const repromptText = null
+  const shouldEndSession = true
+  console.log('SUCCESS: ', speechOutput)
+  callback(sessionAttributes,
+               buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession))
+}
+
+function onError (callback) {
+  const cardTitle = 'Error'
+  const speechOutput = "OrionX API's timed out. Try again later."
+  const repromptText = null
+  const shouldEndSession = true
+  callback({}, buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession))
+}
 
 function getWelcomeResponse (callback) {
-    // If we wanted to initialize the session to have some attributes we could add those here.
-
   var sessionAttributes = {}
   var repromptText = null
 
-  var cardTitle = 'Http GET Example'
+  var cardTitle = 'Welcome!'
 
-    // test http get
-  testGet(function (response) {
-    var speechOutput = 'Response status is ' + response
-    var shouldEndSession = true
+  var speechOutput = 'Hi my name is Stella'
+  var shouldEndSession = true
 
-    callback(sessionAttributes,
-                 buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession))
-  })
-}
-
-function testGet (response) {
-  var http = require('http')
-  var options = {
-    host: 'www.bing.com',
-    port: 80,
-    path: '/',
-    agent: false
-  }
-
-  http.get(options, function (res) {
-    console.log('Response: ' + res.statusCode)
-    response(res.statusCode)
-  }).on('error', function (e) {
-    console.log('Error message: ' + e.message)
-  })
+  callback(sessionAttributes,
+               buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession))
 }
 
 // --------------- Helpers that build all of the responses -----------------------
@@ -126,8 +131,8 @@ function buildSpeechletResponse (title, output, repromptText, shouldEndSession) 
     },
     card: {
       type: 'Simple',
-            // title: "SessionSpeechlet - " + title,
-            // content: "SessionSpeechlet - " + output
+          // title: "SessionSpeechlet - " + title,
+          // content: "SessionSpeechlet - " + output
       title: title,
       content: output
     },
